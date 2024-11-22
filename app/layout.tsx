f3651d9fc49523f0706fc17fb5da54088bd6e272
abderrahimbkd <80970245+abderrahimbkd@users.noreply.github.com>
2024-11-22 22:30:04 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import NavBar from './components/nav/Navbar';
 import Footer from './components/footer/Footer';
 import CartProvider from '@/providers/CartProvider';
 import { Toaster } from 'react-hot-toast';
+import { Suspense } from 'react';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,14 +21,16 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <body className={`${poppins.className} text-slate-700`}>
-        <Toaster toastOptions={{ style: { background: 'rgb(51 65 85)', color: '#fff' } }} />
-        <CartProvider>
-          <div className="flex flex-col min-h-screen">
-            <NavBar />
-            <main className="flex-grow ">{children}</main>
-            <Footer />
-          </div>
-        </CartProvider>
+        <Suspense>
+          <Toaster toastOptions={{ style: { background: 'rgb(51 65 85)', color: '#fff' } }} />
+          <CartProvider>
+            <div className="flex flex-col min-h-screen">
+              <NavBar />
+              <main className="flex-grow ">{children}</main>
+              <Footer />
+            </div>
+          </CartProvider>
+        </Suspense>
       </body>
     </html>
   );
